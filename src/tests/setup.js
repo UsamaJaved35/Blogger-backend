1a66efca9a125dfc49b5b761fdc4f42808cb5079
UsamaJaved007 <usama.javed@dubizzlelabs.com>
2024-06-24 13:08:49 +0500

    time out increased

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const app = require('../server'); // Your Express app
+jest.setTimeout(30000); // Increase timeout to 30 seconds
 
 beforeAll(async () => {
   const url = 'mongodb://127.0.0.1/blogger_test'; // Use a different database for tests
