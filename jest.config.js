a0ee1d0f9735b33dbae49bdb770c2435f514341f
UsamaJaved007 <usama.javed@dubizzlelabs.com>
2024-06-24 12:45:32 +0500

    fixed node versions in ci workflow

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.[t|j]sx?$': 'babel-jest',
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!bson)" // Add this line to avoid ignoring the "bson" package
+  ],
+  testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/src/tests/setup.js'],
   };  
